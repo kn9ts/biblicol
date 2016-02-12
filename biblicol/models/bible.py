af1c97f2c2f6f87d545fa2bc70f3815e767b1449
Eugene Mutai <eugenemutai@gmail.com>
2016-02-12 21:17:25 +0300

    Split the URL to accept a normal or paginated search

--- biblicol/models/bible.py
+++ biblicol/models/bible.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+# from django.db import connection
 
 
 class Bible(models.Model):
@@ -12,8 +13,16 @@ class Bible(models.Model):
     quoted = models.CharField(max_length=100)
     title = models.CharField(max_length=100, null=True)
     abbrev = models.CharField(max_length=100)
-    created_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
-    modified_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now,
+                                        blank=True, null=True)
+    modified_date = models.DateTimeField(default=timezone.now,
+                                         blank=True, null=True)
 
-# DB::statement('ALTER TABLE bible ADD FULLTEXT
-# search(text,keywords,quoted,title)');
+    # def __init__(self):
+    #     """This alters the table to accept FULLTEXT string/column searches"""
+    #     cursor = connection.cursor()
+    #     query = """
+    #     ALTER TABLE biblicol_bible ADD FULLTEXT
+    #     search(text, keywords, quoted, title)
+    #     """
+    #     cursor.execute(query)
--- biblicol/urls.py
+++ biblicol/urls.py
@@ -1,45 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 from .controllers import search
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^search/(?P<param>[\w]+)/(?P<pageno>[0-9]{,3})$', search.index,
-        name='search'),
+    url(r'^search/(?P<param>[\w]+)/', include([
+        url(r'^$', search.index),
+        url(r'^(?P<pageno>[0-9]{,3})/$', search.index),
+    ]), name='search'),
 ]
-
-# // Search path
-# Route::get('search/{param?}/{pageno?}', array('as'=> 'search',
-# 'uses'=> 'SearchController@index'));
-
-# // Resource for reading the Bible
-# Route::resource("read", "ReadingController");
-# Route::resource("read.chapter", "ChaptersController");
-# Route::resource("read.chapter.verse", "SearchController");
-
-# // Expand on a single verse
-# Route::resource("reference", "ReferenceController");
-
-# // Create URL Friendly searches response
-# Route::get('filter', array('as' => 'filter', 'uses' => function () {
-#   $param = preg_replace('/(\s|\%20)/', '_', Input::get('param'));
-#   return Redirect::to("/search/$param");
-# }));
-
-# Route::resource('subscribe', 'SubscribersController', array('except'=> array('delete')));
-# Route::delete('unsubscribe', 'SubscribersController@destroy');
-
-# // eg. http://localhost:8000/someone
-# // Route::get('/{something}', function($something) {
-# //  return Redirect::to("filter")->withInput(array('param' => $something));
-# // });
-
-# // Internal functions
-# Route::get('/books/dump', array('as'=> 'books',
-# 'uses' = > 'BooksController@storeBooksFromAPI'));
-# Route::get('/books/getfrom/api', array('as'=> 'books',
-# 'uses'=> 'BooksController@getBooksFromAPI'));
-
-# Route::get('/hostname', function() {
-#     return [gethostname(), App::environment()];
-# });
