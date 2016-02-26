c36a89b6e41b55a549c86bfbce021c3cc25b812d
Eugene Mutai <eugenemutai@gmail.com>
2016-02-26 19:46:53 +0300

    Added the custom public assets directory to the URL; to be servable in development mode

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # Examples:
@@ -24,3 +25,5 @@ urlpatterns = [
     url(r'^', include('biblicol.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
