7d3988c80e3bc633e57e92ba03d8e701a0b56ff3
Eugene Mutai <eugenemutai@gmail.com>
2016-02-27 10:13:28 +0300

    Renamed/moved the static files to 'public'

--- app/settings.py
+++ app/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/public/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'biblicol/public'),
 )
